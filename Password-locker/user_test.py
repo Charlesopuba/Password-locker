db8ed2ee4fb00abbf3dba74ce110fb8b3a912acc
Charles Opuba <82438457+Charlesopuba@users.noreply.github.com>
2021-08-01 15:06:33 +0300

    Update user_test.py

--- Password-locker/user_test.py
+++ Password-locker/user_test.py
@@ -3,11 +3,11 @@ from  user import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.new_user = User("Collins","Kariuki","collins@gmail.com",)
+        self.new_user = User("Charles","Opuba","charlesopuba@gmail.com",)
     def test1(self):
-        self.assertEqual(self.new_user.f_name,"Collins")
-        self.assertEqual(self.new_user.m_name,"Kariuki")
-        self.assertEqual(self.new_user.e_mail,"collins@gmail.com")
+        self.assertEqual(self.new_user.f_name,"Charles")
+        self.assertEqual(self.new_user.m_name,"Opuba")
+        self.assertEqual(self.new_user.e_mail,"charlesopuba@gmail.com")
     def test_save_user(self):
         self.new_user.save_user()
 
@@ -15,7 +15,7 @@ class TestUser(unittest.TestCase):
         User.userlist = []
     def test_delete_user(self):
         self.new_user.save_user()
-        test_data= User("ariana","gathoni","ariana@gmail.com")
+        test_data= User("charlie","opuba","@gmail.com")
         test_data.save_user()
         self.assertEqual(len(User.user_list),2)
     def test_display_user(self):
